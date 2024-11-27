1e2ee63bcb9473e152f41553c4192b807561adbc
PBerenger <b.pommelet@gmail.com>
2024-11-27 17:10:45 +0100

    271124

---
+++ Projet1/src/config/firebase-config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCe11URZqFyCITEF2wMesN_WlQfYSoYsvM",
+  authDomain: "projet1-e3154.firebaseapp.com",
+  projectId: "projet1-e3154",
+  storageBucket: "projet1-e3154.firebasestorage.app",
+  messagingSenderId: "795650671007",
+  appId: "1:795650671007:web:e1db583fbf768b37ed7daa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ Projet1/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Seo } from "@/ui/components/seo";
+
+export default function Home() {
+	return (
+		<>
+		<Seo title="EvoPort" description="hoho"/>
+		<h1>Hello World</h1>
+		</>
+	)
+}
